b36a6c8718b3ef2c77e3b21ecee6b32b7caba76c
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-07 14:40:27 +0900

    ✨ feat: msw 이용mock server 구현

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,22 @@
+import { rest } from 'msw';
+
+import { MOCK_API_URL, MOCK_API_PATH } from '../apis/apiConfig';
+import data from './db';
+
+export const handlers = [
+  rest.get(`${MOCK_API_URL}${MOCK_API_PATH.SICK}`, (req, res, ctx) => {
+    const searchedNoun = req.url.searchParams.get('q');
+
+    if (!searchedNoun) {
+      return res(ctx.status(200), ctx.json(data));
+    }
+
+    const filteredData = {
+      sick: data.sick.filter(item => {
+        return item.sickNm.includes(searchedNoun);
+      }),
+    };
+
+    return res(ctx.status(200), ctx.json(filteredData));
+  }),
+];
